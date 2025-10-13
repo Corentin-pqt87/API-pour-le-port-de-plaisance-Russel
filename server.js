1ee2eed4965574573c0f00b588a7c5ef29e79ae8
PAQUET_Corentin <propcorentin87@gmail.com>
2025-10-13 15:51:58 +0200

    page d'acceuil

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// si le lien est '/' alors :
+app.get('/html',(req, res) => {
+    res.type('text/html');
+    res.send('<h1>API pour le port de plaisance Russel</h1>');
+})
+
+// si la page n'est pas trouver
+app.use( (req , res ) => {
+    res.type('text/plain');
+    res.status( 404 );
+    res.send('404 page non trouvée');
+})
+
 app.listen(port, () => {
     console.log('Server app listening on port ' + port);
 })
