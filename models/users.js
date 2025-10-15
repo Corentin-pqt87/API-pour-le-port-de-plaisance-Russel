97a108047358ddff6f03c9482cc908ac7621cec1
Corentin-pqt87 <propcorentin87@gmail.com>
2025-10-15 17:07:21 +0200

    add insex.js + model/users

--- models/users.js
+++ models/users.js
@@ -1,9 +1,40 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs')
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+const User = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    require: [true, 'Le nom est requis']
+  },
+  firstname: { 
+    type: String,
+    trim: true
+  },
+  email: { 
+    type: String,
+    trim: true,
+    require: [true, 'L email est requis'],
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    trim: true
+  }, 
+}, {
+  timestamps: true
 });
 
-module.exports = mongoose.model('User', UserSchema);
+User.pre('save', function(next){
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  this.password = bcrypt.hashSync(this.password, 10);
+
+  next();
+});
+
+module.exports = mongoose.model('User', User);
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const userRoute = require('../routes/users');
+const { version } = require('mongoose');
+
+router.get('/', async (requestAnimationFrame, res) => {
+    res.status(200).json({
+        nam: process.env.APP_NAME,
+        version: '1.0',
+        status : 200,
+        message: 'Bienvenue sur l\'API'
+    });
+});
+
+router.use('/users', userRoute);
+
+module.exports = router;
