1278320300eb29a7b567d34e9d98c86ecfc5b98c
Corentin-pqt87 <propcorentin87@gmail.com>
2025-11-05 13:24:42 +0100

    middleware

--- routes/users.js
+++ routes/users.js
@@ -2,27 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 const userService  = require('../services/users');
+const { useReducer } = require('react');
 
 // GET /users
 router.get('/', (req, res) => {
   res.json({ message: 'Users API endpoint OK' });
 });
 // GET /users/:id
-router.get('/:id', userService.getById);
-
-// POST /users/authenticate
-router.post('/authenticate', userService.authenticate);
+router.get('/:id',private.checkJWT , userService.getById);
 
 // PUT /users/add
 router.put('/add', userService.add);
 
-// PATCH /users/:id
-router.patch('/:id', userService.update);
+// PATCH /users/update
+router.patch('/update', private.checkJWTn, userService.update)
 
-// DELETE /users/:id
-router.delete('/:id', userService.delete);
+// DELETE /users/delete
+router.delete('/delete', private.checkJWTn, userService.delete);
 
-// route d'authentification
+// POST /users/authenticate
 router.post('/authenticate', userService.authenticate);
 
 module.exports = router;
