46c8df4628a78851ebdcf09a90760b93441e3b6a
Corentin-pqt87 <propcorentin87@gmail.com>
2025-11-07 10:36:30 +0100

    changement de port préparation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import { login } from "./services/api";
 
 function App() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLogin() {
+    const res = await login(email, password);
+    console.log(res);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Connexion</h1>
+      <input placeholder="email" onChange={e => setEmail(e.target.value)} />
+
+      <input placeholder="mot de passe" type="password" onChange={e => setPassword(e.target.value)} />
+      
+      <button onClick={handleLogin}>Se connecter</button>
     </div>
   );
 }
