47693a6eac9975fdfc5a3e70b85702a86aa51af3
Corentin-pqt87 <propcorentin87@gmail.com>
2025-11-07 11:33:45 +0100

    modif du port de l'api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "http://localhost:5000";
 
 export async function login(email, password) {
   const res = await fetch(`${API_URL}/users/authenticate`, {
