e06a2955bdb0ba1ebe0055b8abecbbe04b69b610
Corentin-pqt87 <propcorentin87@gmail.com>
2025-11-14 14:00:26 +0100

    supprimer et modifier une reservation

--- backend/services/reservation.js
+++ backend/services/reservation.js
@@ -17,3 +17,39 @@ exports.add = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+exports.update = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const reservation = await Reservation.findById(id);
+    if (!reservation) {
+      return res.status(404).json({ status: "reservation_not_found" });
+    }
+
+    Object.keys(req.body).forEach((key) => {
+      if (req.body[key] !== undefined) {
+        reservation[key] = req.body[key];
+      }
+    });
+
+    await reservation.save();
+    res.status(200).json(reservation);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.delete = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const reservation = await Reservation.findById(id);
+    if (!reservation) {
+      return res.status(404).json({ status: "reservation_not_found" });
+    }
+
+    await Reservation.deleteOne({ _id: id });
+    res.status(204).json({ status: "delete_ok" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
