258d2a4fdf6e33eb3a7c2ab8ec2ed3374b638772
Corentin-pqt87 <propcorentin87@gmail.com>
2025-11-14 13:53:23 +0100

    ajouter et récupérer une réservation

---
+++ backend/services/reservation.js
@@ -0,0 +1,19 @@
+const Reservation = require('../models/reservation');
+
+exports.getAll = async (req, res) => {
+  try {
+    const reservations = await Reservation.find();
+    res.status(200).json(reservations);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.add = async (req, res) => {
+  try {
+    const reservation = await Reservation.create(req.body);
+    res.status(201).json(reservation);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
