b9d43438f1196548e42264881498e129679c9888
Corentin-pqt87 <propcorentin87@gmail.com>
2025-11-14 14:22:24 +0100

    correction singulier

--- backend/services/reservation.js
+++ backend/services/reservation.js
@@ -2,8 +2,8 @@ const Reservation = require('../models/reservation');
 
 exports.getAll = async (req, res) => {
   try {
-    const reservations = await Reservation.find();
-    res.status(200).json(reservations);
+    const reservation = await Reservation.find();
+    res.status(200).json(reservation);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
